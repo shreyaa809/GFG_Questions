1f515222b7b884fe892f2095799be4fb9114a8e2
Shreya Shukla <shreyashukla218@gmail.com>
2026-07-10 20:21:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological Sort/topological-sort.cpp
+++ Difficulty: Medium/Topological Sort/topological-sort.cpp
@@ -1,6 +1,6 @@
 class Solution {
   public:
-    void findAdj(vector<vector<int>>&edges,vector<vector<int>>&adj)
+    void findAdj(int V, vector<vector<int>>&edges,vector<vector<int>>&adj)
     {
         for (auto &it:edges)
         {
@@ -10,37 +10,46 @@ class Solution {
         }
         return;
     }
-    void Dfs(vector<vector<int>>&adj,vector<bool>&vis,int i,stack<int>&st)
+    void findIndegree(int V, vector<vector<int>>&adj,vector<int>&inD,int u)
     {
-        vis[i]=true;
-        for (auto &it:adj[i])
+        for (auto &it:adj[u])
         {
-            if (!vis[it])
-            Dfs(adj,vis,it,st);
+            inD[it]++;
         }
-        st.push(i);
         return;
     }
     vector<int> topoSort(int V, vector<vector<int>>& edges) 
     {
         // code here
-        int n=edges.size();
+        //i havee to do using bfs->kahn's algo->indegree, queue use hoga
         vector<vector<int>>adj(V);
        
-        findAdj(edges,adj);
-        vector<bool>vis(V);
-        stack<int>st;
+        findAdj(V,edges,adj);
+        vector<int>inD(V);
+        for (int i=0;i<V;i++)
+        findIndegree(V,adj,inD,i);
+        
+        queue<int>q;
+        vector<int>ans;
         for (int i=0;i<V;i++)
         {
-            if (!vis[i])
-            Dfs(adj,vis,i,st);
+            if (inD[i]==0)
+            q.push(i);
         }
-        vector<int>ans;
-        while (!st.empty())
+        
+        while (!q.empty())
         {
-            ans.push_back(st.top());
-            st.pop();
+            int curr=q.front();
+            ans.push_back(curr);
+            q.pop();
+            for (auto &it:adj[curr])
+            {
+                inD[it]--;
+                if (inD[it]==0)
+                q.push(it);
+            }
         }
         return ans;
+        
     }
 };
